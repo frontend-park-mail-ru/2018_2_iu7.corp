675294925309cbf48a9a84644fa186c04f597bbe
nikitarub <nikita.rub@yandex.ru>
2018-12-12 09:34:37 +0300

    Added resizable canvas

--- src/js/views/gameViews/singleplayer/SingleGameView.js
+++ src/js/views/gameViews/singleplayer/SingleGameView.js
@@ -9,18 +9,74 @@ const data = {};
 
 data.headerMenu = [
 	{
-		label: 'Время',
+		label: '⏱',
 		data: '00'
 	},
 	{
-		label: 'Очки',
-		data: '00'
-	},
-	{
-		label: 'Мобы',
+		label: '👾',
 		data: '4'
 	}
 ];
+data.helpValues = [
+	{
+		label: 'Цель игры',
+		data: 'Уничтожить всех врагов'
+	},
+	{
+		label: 'Перемещение персонажа',
+		data: '"wasd" или стрелочки'
+	},
+	{
+		label: 'Поставить бомбу',
+		data: '"f"'
+	},
+	{
+		label: 'Радиус бомбы',
+		data: '2 клетки'
+	},
+];
+
+const resize = function () {
+	// let y;
+	// let x;
+	canvas1 = document.getElementById("canvas1");
+	canvas2 = document.getElementById("canvas2");
+	
+	canvas1.width = window.innerWidth;
+	canvas1.height = window.innerHeight;
+
+	canvas2.width = window.innerWidth;
+	canvas2.height = window.innerHeight;
+
+	// const bigStep = 100;
+	// const smallStep = 10;
+	// context.beginPath();
+	// context.strokeStyle = '#eeeeee';
+	// for (x = 0; x < canvas.width; x += smallStep) {
+	// 	context.moveTo(x, 0);
+	// 	context.lineTo(x, canvas.height);
+	// }
+	// for (y = 0; y < canvas.height; y += smallStep) {
+	// 	context.moveTo(0, y);
+	// 	context.lineTo(canvas.width, y);
+	// }
+	// context.stroke();
+
+	// context.beginPath();
+	// context.strokeStyle = '#aaaaaa';
+	// for (x = 0; x < canvas.width; x += bigStep) {
+	// 	context.moveTo(x, 0);
+	// 	context.lineTo(x, canvas.height);
+	// }
+	// for (y = 0; y < canvas.height; y += bigStep) {
+	// 	context.moveTo(0, y);
+	// 	context.lineTo(canvas.width, y);
+	// }
+	// context.stroke();
+
+	// context.strokeStyle = '#ff0000';
+};
+
 
 export default class SingleGameView extends BaseView {
 	constructor () {
@@ -38,6 +94,7 @@ export default class SingleGameView extends BaseView {
 	}
 
 	render (user) {
+		
 		if (!user.is_authenticated) {
 			data.headerValues = notAuthMenuHeader();
 			super.render(data);
@@ -45,11 +102,23 @@ export default class SingleGameView extends BaseView {
 			data.headerValues = authMenuHeader(user.id);
 			super.render(data);
 		}
+		this.showInfo();
+		
 		SingleGame.init();
 		SingleGame.start();
+		// resize();
 	}
 
+	showInfo() {
+		document.getElementById("dropdown-game-info").style.height = "100%";
+	};
+
+	hideInfo() {
+		document.getElementById("dropdown-game-info").style.height = "0%";
+	};
+
 	registerActions () {
+		// this.viewDiv.onload(this.showInfo());
 		// this.viewDiv.addEventListener('click', this._navigationController.keyPressedCallback);
 	}
 }
