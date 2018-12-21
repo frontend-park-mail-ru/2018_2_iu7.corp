e95dc878ca82ca47936fb8db2dcd276813e3dd8c
Ermako27 <ermakov.1828@gmail.com>
2018-12-21 05:37:42 +0300

    reduce Bus

--- src/js/modules/Bus.js
+++ src/js/modules/Bus.js
@@ -1,38 +1,3 @@
-import { type } from "os";
-
-// export class Bus {
-// 	constructor () {
-// 		this._listeners = {};
-// 	}
-
-// 	on (event, callback) {
-// 		if (!this._listeners[event]) {
-// 			this._listeners[event] = [];
-// 		}
-// 		this._listeners[event].push({ callback });
-// 	}
-
-// 	off (event, callback) {
-// 		this._listeners[event] = this._listeners[event].filter((listener) => {
-// 			return listener.callback !== callback;
-// 		});
-
-// 	}
-
-// 	totalOff (event) {
-// 		this._listeners[event] = [];
-// 	}
-
-// 	emit (event, data) {
-// 		this._listeners[event].forEach((listener) => {
-// 			listener.callback(data);
-// 		});
-// 	}
-// }
-
-// export default new Bus();
-
-
 export class Bus {
 	constructor () {
 		this._listeners = {};
